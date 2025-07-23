07c5641c875d75b978c6a5ce65e1a89a20c55b93
MICHAELKITH <michaelkithinji27@gmail.com>
2025-07-24 00:05:01 +0300

    Add go packets

--- 05-packet-capturing/main.go
+++ 05-packet-capturing/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
     "fmt"
-    // "github.com/google/gopacket"
-    // "github.com/google/gopacket/pcap"
+    "github.com/google/gopacket"
+    "github.com/google/gopacket/pcap"
 )
 
 func main() {
