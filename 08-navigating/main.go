f5ecd5d4fd5888940179b70a615e897162110925
MICHAELKITH <michaelkithinji27@gmail.com>
2025-08-04 23:47:02 +0300

    Navigating files

---
+++ 08-navigating/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "os"
+    "path/filepath"
+)
+
+func listFiles(dir string) {
+    files, err := ioutil.ReadDir(dir)
+    if err != nil {
+        fmt.Println("Error reading directory:", err)
+        return
+    }
+    for _, file := range files {
+        fmt.Println(filepath.Join(dir, file.Name()))
+    }
+}
+
+func main() {
+    listFiles("/home/user")
+}
